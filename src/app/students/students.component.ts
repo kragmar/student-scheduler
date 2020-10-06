de51167a8610d8cc881f668bc919ff29d5cf97df
Nagy Márk <kragmar97@gmail.com>
2020-10-06 17:16:52 +0200

    feat: add form and form controls

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.css'],
 })
 export class StudentsComponent implements OnInit {
-  constructor() {}
+  studentForm = this.fb.group({
+    name: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  onSubmit() {}
 }
