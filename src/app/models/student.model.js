6f91b31d601f605a91021b810ecdf6853692930e
Nagy Márk <kragmar97@gmail.com>
2020-10-13 15:17:58 +0200

    refactor: change student schema structure

--- src/app/models/student.model.js
+++ src/app/models/student.model.js
@@ -1,4 +1,4 @@
-module.exports = (mongoose) => {
+/* module.exports = (mongoose) => {
   const Student = mongoose.model(
     "student",
     mongoose.Schema(
@@ -14,3 +14,29 @@ module.exports = (mongoose) => {
 
   return Student;
 };
+ */
+
+const mongoose = require("mongoose");
+
+var studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    telNum: {
+      type: String,
+      required: true,
+    },
+    birthDate: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+mongoose.model("Student", studentSchema);
