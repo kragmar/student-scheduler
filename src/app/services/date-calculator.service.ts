04e59a40dd6286e57dc0aa1e41d733a5bb35c9fa
Nagy Márk <kragmar97@gmail.com>
2020-11-17 23:16:27 +0100

    feat: add date calculator service

---
+++ src/app/services/date-calculator.service.ts
@@ -0,0 +1,76 @@
+import { Lesson, LessonService } from './lesson.service';
+import { Injectable } from '@angular/core';
+import * as dayjs from 'dayjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DateCalculatorService {
+  constructor(private lessonService: LessonService) {}
+
+  get times() {
+    return [
+      '12:50',
+      '13:40',
+      '14:30',
+      '15:20',
+      '16:10',
+      '17:00',
+      '17:50',
+      '18:40',
+    ];
+  }
+
+  get days() {
+    return ['Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek'];
+  }
+
+  getEmptyDate() {
+    let date = new Date();
+    date.setHours(12, 50, 0, 0);
+    let day = dayjs(date);
+    day = day.add(1, 'day');
+
+    /**
+     * Set empty date to tomorrow
+     * If it's Friday then set it to Monday
+     */
+    /* if (date.getDay() === 4) {
+      date.setDate(date.getDate() + 3);
+    } else {
+      date.setDate(date.getDate() + 1);
+    } */
+
+    console.log(day);
+
+    let datetime = date.getTime();
+
+    let lessons: Lesson[] = [];
+    this.lessonService.findAllAfterToday(datetime).subscribe(
+      (res) => {
+        lessons = res;
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        let emptyDates: Date[];
+        console.log(day.toDate().getTime());
+        for (const lesson of lessons) {
+          let date = dayjs(lesson.date);
+          date = date.add(-1, 'hour');
+
+          if (day.isSame(date)) {
+            day = day.add(50, 'minute');
+            console.log(day);
+            console.log('ASD');
+            console.log(lesson);
+          } else {
+            console.log('DSA');
+            console.log(lesson);
+          }
+        }
+      }
+    );
+  }
+}
