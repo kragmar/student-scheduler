b3830e396c6aa65f477964890815b70452fe2a30
Nagy Márk <kragmar97@gmail.com>
2020-11-30 02:45:35 +0100

    fix: disable jwt declaration too

--- src/app/routes/index.routes.js
+++ src/app/routes/index.routes.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("express-jwt");
 
 /**
- * TODO: route authentication
+ * TODO: Route authentication
  */
-/* const auth = jwt({
+/* const jwt = require("express-jwt");
+const auth = jwt({
   secret: "MY_SECRET",
   userProperty: "payload",
   algorithms: ["HS256"],
 }); */
+
 const ctrlAuth = require("../controllers/authentication.controller");
 
 // authentication and registration
