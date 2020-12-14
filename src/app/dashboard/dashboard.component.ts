5c1a9b4fe7f776b683e07560fe1bcca642b85b7c
Nagy Márk <kragmar97@gmail.com>
2020-12-14 20:05:19 +0100

    feat: add lesson service to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
+import { Lesson } from './../services/lesson.service';
 import { Component, OnInit } from '@angular/core';
+import { LessonService } from '../services/lesson.service';
 
 @Component({
   selector: 'sg-app-dashboard',
