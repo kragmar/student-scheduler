b0b31b045249642633c494ff40485345ea0e0df6
Nagy Márk <kragmar97@gmail.com>
2020-12-26 23:01:18 +0100

    feat: add daily schedule component

---
+++ src/app/dashboard/daily-schedule/daily-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-daily-schedule',
+  templateUrl: './daily-schedule.component.html',
+  styleUrls: ['./daily-schedule.component.css'],
+})
+export class DailyScheduleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
