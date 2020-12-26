51d5bc6b487ce8b50f6814e154fa3b6c53c2b4da
Nagy Márk <kragmar97@gmail.com>
2020-12-26 23:01:39 +0100

    feat: import daily schedule in dashboard module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,9 +5,14 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { WeeklyCalendarComponent } from './weekly-calendar/weekly-calendar.component';
 import { SharedModule } from '../shared/shared.module';
+import { DailyScheduleComponent } from './daily-schedule/daily-schedule.component';
 
 @NgModule({
-  declarations: [DashboardComponent, WeeklyCalendarComponent],
+  declarations: [
+    DashboardComponent,
+    WeeklyCalendarComponent,
+    DailyScheduleComponent,
+  ],
   imports: [CommonModule, SharedModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
