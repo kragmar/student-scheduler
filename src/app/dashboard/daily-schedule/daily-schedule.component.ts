96af1f07252fa575ed89706361496288e248fe1d
Nagy Márk <kragmar97@gmail.com>
2020-12-27 21:36:43 +0100

    feat: add lesson grouping for daily schedule

--- src/app/dashboard/daily-schedule/daily-schedule.component.ts
+++ src/app/dashboard/daily-schedule/daily-schedule.component.ts
@@ -8,12 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DailyScheduleComponent implements OnInit {
   lessons: Lesson[];
+  groupedLessons: Lesson[][] = new Array();
 
   constructor(private lessonService: LessonService) {}
 
   ngOnInit(): void {
-    this.lessonService
-      .findAllToday()
-      .subscribe((data) => (this.lessons = data));
+    this.lessonService.findAllToday().subscribe((data) => {
+      this.lessons = data;
+      this.groupLessons();
+    });
+  }
+
+  groupLessons(): void {
+    let j = 0;
+    let duplicateLessons = new Array<Lesson>();
+    for (let i = 0; i < this.lessons.length; i++) {
+      const compareToDate = new Date(this.lessons[i].date);
+      const lessonDate = new Date(this.lessons[j].date);
+
+      if (lessonDate.getTime() === compareToDate.getTime()) {
+        duplicateLessons.push(this.lessons[i]);
+        if (i + 1 === this.lessons.length) {
+          this.groupedLessons.push(duplicateLessons);
+        }
+      } else {
+        j = i;
+        i -= 1;
+        this.groupedLessons.push(duplicateLessons);
+        duplicateLessons = new Array<Lesson>();
+      }
+    }
   }
 }
