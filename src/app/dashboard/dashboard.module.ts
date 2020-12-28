417366c8e251aba9570dcecb6eb36d6fa51fb8de
Nagy Márk <kragmar97@gmail.com>
2020-12-28 02:00:55 +0100

    feat: add message board component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -6,12 +6,14 @@ import { DashboardComponent } from './dashboard.component';
 import { WeeklyCalendarComponent } from './weekly-calendar/weekly-calendar.component';
 import { SharedModule } from '../shared/shared.module';
 import { DailyScheduleComponent } from './daily-schedule/daily-schedule.component';
+import { MessageBoardComponent } from './message-board/message-board.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     WeeklyCalendarComponent,
     DailyScheduleComponent,
+    MessageBoardComponent,
   ],
   imports: [CommonModule, SharedModule, DashboardRoutingModule],
 })
---
+++ src/app/dashboard/message-board/message-board.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-message-board',
+  templateUrl: './message-board.component.html',
+  styleUrls: ['./message-board.component.css'],
+})
+export class MessageBoardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
