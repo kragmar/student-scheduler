1b7b5b3dbf3ba1b8af8b264a6508d8450e7e25a4
Nagy Márk <kragmar97@gmail.com>
2020-12-29 03:25:41 +0100

    refactor: rename to message dialog

--- src/app/dashboard/message-dialog/message-dialog.component.spec.ts
+++ src/app/dashboard/message-dialog/message-dialog.component.spec.ts
@@ -1,20 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NewMessageDialogComponent } from './new-message-dialog.component';
+import { MessageDialogComponent } from './message-dialog.component';
 
 describe('NewMessageDialogComponent', () => {
-  let component: NewMessageDialogComponent;
-  let fixture: ComponentFixture<NewMessageDialogComponent>;
+  let component: MessageDialogComponent;
+  let fixture: ComponentFixture<MessageDialogComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewMessageDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [MessageDialogComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NewMessageDialogComponent);
+    fixture = TestBed.createComponent(MessageDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
