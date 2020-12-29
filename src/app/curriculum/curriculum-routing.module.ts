48b5203c461d38d6e4de931e84f6560510b066e4
Nagy Márk <kragmar97@gmail.com>
2020-12-29 05:50:01 +0100

    feat: add curriculum route to app

--- src/app/curriculum/curriculum-routing.module.ts
+++ src/app/curriculum/curriculum-routing.module.ts
@@ -1,9 +1,14 @@
+import { AuthGuard } from './../core/services/auth.guard';
 import { CurriculumComponent } from './curriculum.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '/curriculum', component: CurriculumComponent },
+  {
+    path: 'curriculum',
+    component: CurriculumComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
