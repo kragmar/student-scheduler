e69cfb9cd2f55317fa4c1312bfd6c3fb572103ea
Nagy Márk <kragmar97@gmail.com>
2020-12-29 06:29:09 +0100

    feat: get curriculum data from service

--- src/app/curriculum/curriculum.component.ts
+++ src/app/curriculum/curriculum.component.ts
@@ -1,3 +1,7 @@
+import {
+  Curriculum,
+  CurriculumService,
+} from './../core/services/curriculum.service';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
@@ -7,7 +11,15 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CurriculumComponent implements OnInit {
-  constructor() {}
+  curriculums: Curriculum[];
 
-  ngOnInit(): void {}
+  columns: string[] = ['title', 'subtitle', 'desc', 'group'];
+
+  constructor(private curriculumService: CurriculumService) {}
+
+  ngOnInit(): void {
+    this.curriculumService
+      .findAll()
+      .subscribe((data) => (this.curriculums = data));
+  }
 }
