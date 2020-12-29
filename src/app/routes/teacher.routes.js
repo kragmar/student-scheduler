369ec383d0988874c2358d472f66b609a2c2dbf0
Nagy Márk <kragmar97@gmail.com>
2020-12-29 02:03:35 +0100

    feat: update teacher api routes

--- src/app/routes/teacher.routes.js
+++ src/app/routes/teacher.routes.js
@@ -1,23 +1,19 @@
-module.exports = (app) => {
-  const teacher = require("../controllers/teacher.controller");
+const router = require("express").Router();
+const teacherCtrl = require("../controllers/teacher.controller");
 
-  var router = require("express").Router();
+// Create a new teacher
+router.post("/", teacherCtrl.create);
 
-  // Create a new teacher
-  router.post("/", teacher.create);
+// Retrieve all teacher
+router.get("/", teacherCtrl.findAll);
 
-  // Retrieve all teacher
-  router.get("/", teacher.findAll);
+// Retrieve a single teacher with id
+router.get("/:id", teacherCtrl.findOne);
 
-  // Retrieve a single teacher with id
-  router.get("/:id", teacher.findOne);
+// Update a teacher with id
+router.put("/:id", teacherCtrl.update);
 
-  // Update a teacher with id
-  router.put("/:id", teacher.update);
+// Delete a teacher with id
+router.delete("/:id", teacherCtrl.delete);
 
-  // Delete a teacher with id
-  router.delete("/:id", teacher.delete);
-
-  // App uses this route
-  app.use("/api/teachers", router);
-};
+module.exports = router;
