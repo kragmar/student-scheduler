446c9a21b959b3814a9fd6350aea26dcc0ea3706
Nagy Márk <kragmar97@gmail.com>
2020-12-30 16:08:10 +0100

    feat: add curriculum to lesson controller

--- src/app/api/controllers/lesson.controller.js
+++ src/app/api/controllers/lesson.controller.js
@@ -10,6 +10,7 @@ module.exports.create = (req, res) => {
     recurring: req.body.recurring,
     studentId: req.body.studentId,
     teacherId: req.body.teacherId,
+    curriculum: req.body.curriculum,
   });
 
   // Save lesson in the db
