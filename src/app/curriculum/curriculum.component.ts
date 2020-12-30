2ad6c8b9d0765ce323b91466e89f8b7be008528a
Nagy Márk <kragmar97@gmail.com>
2020-12-30 04:26:56 +0100

    feat: add curriculum table sorting, filtering and curriculum editing

--- src/app/curriculum/curriculum.component.ts
+++ src/app/curriculum/curriculum.component.ts
@@ -1,25 +1,78 @@
+import { OkDialogComponent } from './../shared/components/ok-dialog/ok-dialog.component';
+import { CurriculumDialogComponent } from './curriculum-dialog/curriculum-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 import {
   Curriculum,
   CurriculumService,
 } from './../core/services/curriculum.service';
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'curriculum-curriculum',
   templateUrl: './curriculum.component.html',
   styleUrls: ['./curriculum.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CurriculumComponent implements OnInit {
+export class CurriculumComponent implements OnInit, AfterViewInit {
   curriculums: Curriculum[];
+  editing = false;
 
-  columns: string[] = ['title', 'subtitle', 'desc', 'group'];
+  dataSource = new MatTableDataSource<Curriculum>();
+  displayedColumns: string[] = ['title', 'subtitle', 'desc', 'group'];
 
-  constructor(private curriculumService: CurriculumService) {}
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private curriculumService: CurriculumService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-    this.curriculumService
-      .findAll()
-      .subscribe((data) => (this.curriculums = data));
+    this.curriculumService.findAll().subscribe((data) => {
+      this.dataSource.data = data as Curriculum[];
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  openCurriculumDialog(curriculum?: Curriculum): void {
+    const dialogRef = this.dialog.open(CurriculumDialogComponent, {
+      width: 'fit-content',
+      data: { curriculum: curriculum, editing: this.editing },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      let msg;
+
+      if (result === 'SUCCESS') {
+        if (this.editing) {
+          msg = 'Tananyag frissítve!';
+        } else {
+          msg = 'Új tananyag létrehozva!';
+        }
+      } else if (result === 'DELETION') {
+        msg = 'Tananyag törölve!';
+      } else {
+        return;
+      }
+
+      this.openOkDialog(msg);
+    });
+  }
+
+  openOkDialog(message: string): void {
+    const dialogRef = this.dialog.open(OkDialogComponent, {
+      width: 'fit-content',
+      data: { message: message },
+      panelClass: 'ok-dialog',
+    });
   }
 }
