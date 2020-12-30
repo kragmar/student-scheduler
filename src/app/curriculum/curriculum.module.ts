867bdc7e33e4c266e076d6d63f0f062bf8579c57
Nagy Márk <kragmar97@gmail.com>
2020-12-30 04:25:03 +0100

    feat: import curriculum dialog into module

--- src/app/curriculum/curriculum.module.ts
+++ src/app/curriculum/curriculum.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CurriculumRoutingModule } from './curriculum-routing.module';
 import { CurriculumComponent } from './curriculum.component';
 import { SharedModule } from '../shared/shared.module';
+import { CurriculumDialogComponent } from './curriculum-dialog/curriculum-dialog.component';
 
 @NgModule({
-  declarations: [CurriculumComponent],
+  declarations: [CurriculumComponent, CurriculumDialogComponent],
   imports: [CommonModule, SharedModule, CurriculumRoutingModule],
 })
 export class CurriculumModule {}
