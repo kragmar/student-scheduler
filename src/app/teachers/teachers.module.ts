9a95f9cfe45ce462051ec6c6f834f847b57c9b67
Nagy Márk <kragmar97@gmail.com>
2020-12-31 06:25:28 +0100

    refactor: remove teacher dialog from teacher module

--- src/app/teachers/teachers.module.ts
+++ src/app/teachers/teachers.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 
 import { TeachersRoutingModule } from './teachers-routing.module';
 import { TeachersComponent } from './teachers.component';
-import { TeacherDialogComponent } from './teacher-dialog/teacher-dialog.component';
 
 @NgModule({
-  declarations: [TeachersComponent, TeacherDialogComponent],
+  declarations: [TeachersComponent],
   imports: [CommonModule, SharedModule, TeachersRoutingModule],
 })
 export class TeachersModule {}
